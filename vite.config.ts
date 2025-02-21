ffada8e0003ea10abed7410304fd04ab3252b093
ahmedHarb <aharb@averroes.ai>
2025-02-21 17:05:32 +0200

    chore: Add vite checker plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,23 @@ import { defineConfig } from "vite";
 import TurboConsole from "unplugin-turbo-console/vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import checker from "vite-plugin-checker";
+import removeConsole from "vite-plugin-remove-console";
 
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), TurboConsole()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    TurboConsole(),
+    checker({
+      typescript: true,
+      // eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"' },
+    }),
+    removeConsole(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "./src"),
