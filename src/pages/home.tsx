ef7b189f548a6667b922bb631fc81973557645e7
ahmedHarb <aharb@averroes.ai>
2025-02-21 17:06:49 +0200

    refactor: Remove app component file

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
 
 const Home = () => {
-  console.log("Home");
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold ">Home Directory</h1>
-      <Button className="hover:animate-pulse w-full">click here</Button>
+      <Button className="hover:animate-pulse w-full text-black">
+        click here to check
+      </Button>
     </div>
   );
 };
